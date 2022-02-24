7e98ba942a67d1591528492437dbb026888b0e3f
Lygia Dias <lygiiadias@gmail.com>
2022-02-24 03:27:21 -0300

    Tentativa 1.000

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -8,8 +8,9 @@ const listSale = async (req, res) => {
   const getSaleId = async (req, res) => {
       const { id } = req.params;
       const responses = await salesService.getSaleId(id);
+      
       if (!responses) return res.status(404).json({ message: 'Sale not found' });
-      return res.status(200).json(responses);
+      res.status(200).json(responses);
     };
 
   module.exports = { listSale, getSaleId };
