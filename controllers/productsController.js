aa16f754523b8b131e98ba85e9aab4864861a333
Lygia Dias <lygiiadias@gmail.com>
2022-02-24 02:30:28 -0300

    Requisito 2 again

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -13,7 +13,7 @@ const listProducts = async (_req, res, next) => {
     try {
       const { id } = req.params;
       const products = await productService.getProductId(id);
-      if (!products) return res.status(404).json({ message: 'Product not found' });
+      if (!products.length) return res.status(404).json({ message: 'Product not found' });
       return res.status(200).json(products);
     } catch (e) {
       next(e);
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -13,7 +13,7 @@ const listSale = async (_req, res, next) => {
     try {
       const { id } = req.params;
       const responses = await salesService.getSaleId(id);
-      if (!responses) return res.status(404).json({ message: 'Sale not found' });
+      if (!responses.length) return res.status(404).json({ message: 'Sale not found' });
       return res.status(200).json(responses);
     } catch (e) {
       next(e);
